06438b981220cab3ce58b321b9be1b6077c1eccf
laron58 <141530865+laron58@users.noreply.github.com>
2023-11-15 11:57:34 -0600

    Update masterCrack.py
    + added MD5 and SHA256 hashing/cracking

--- masterCrack.py
+++ masterCrack.py
@@ -1,12 +1,16 @@
 import sys
+import hashlib
 #count = 0
 found = False
 run = True
+#hMode = 0
 
 def main(inMode = -1):
     global count
     global found
     global pwd
+    global hMode
+    hMode = 0
 
     print("~~~~CRACKERMACHINE 9000~~~~")
     if inMode == -1:
@@ -23,30 +27,36 @@ def main(inMode = -1):
             mode = int(input("Enter 0 to change password\n"\
                             "Enter 1 to use dictionary\n"\
                             "Enter 2 to brute force\n"\
-                            "Enter 3 to quit\n"))
-            checkMode(mode)
+                            "Enter 3 to convert to MD5\n"\
+                            "Enter 4 to convert to SHA256\n"\
+                            "Enter 5 to quit\n"))
+            checkMode(mode, hMode)
             
-def dictCrack(pwd):
+def dictCrack(pwd, hMode):
     global count
     passList = open("passList.txt", "r").read()
     list = passList.splitlines()
-
     for guess in list:
-        #print(guess)
         count += 1
-        if guess == pwd:
+        if hMode > 2:
+            hGuess = toHash(guess, hMode)
+        else:
+            hGuess = guess
+        #print(hGuess)
+        if hGuess == pwd:
             print("\nCracked password: ", guess)
             print(count, "tries\n")
             break
     if count == 10000:
         print("\nDictionary failed :( \nMaybe try brute forcing?\n")
 
-def bruteCrack(pwd, size, guess = ""):
+def bruteCrack(pwd, size, hMode, guess = ""):
     global count
     global found
-
     if size == 0:
         count += 1
+        if hMode > 2:
+            guess = toHash(guess, hMode)
         #print(guess)
         if guess == pwd:
             print("\nCracked password: " + guess)
@@ -59,25 +69,38 @@ def bruteCrack(pwd, size, guess = ""):
             if found:
                 return
             newGuess = guess + chr(char)
-            bruteCrack(pwd, size - 1, newGuess)
-            
-def checkMode(mode):
-    global pwd
+            bruteCrack(pwd, size - 1, hMode, newGuess)
 
+def toHash(pwd, mode):
+    global hMode
+    hMode = mode
+    if mode == 3:
+        hash = hashlib.md5(pwd.encode()).hexdigest()
+    elif mode == 4:
+        hash = hashlib.sha256(pwd.encode()).hexdigest()
+    return hash
+    
+            
+def checkMode(mode, hMode = 0):
+    global pwd
     if int(mode) == 0:
         pwd = input("\nEnter new password: ")
     elif int(mode) == 1:
         #count = 0
-        dictCrack(pwd)
+        dictCrack(pwd, hMode)
     elif int(mode) == 2:
         #count = 0
         global found
         found = False
         for x in range(1, 21):
-            bruteCrack(pwd, x)  
-    elif int(mode) == 3:
+            bruteCrack(pwd, x, hMode)  
+    elif int(mode) == 5:
         global run
         run = False
+    else:
+        pwd = toHash(pwd, mode)
+        print("\nHashed password:", pwd)
+
 
 if __name__ == '__main__':
     #print("~~~~CRACKERMACHINE 9000~~~~")
