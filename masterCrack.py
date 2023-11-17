becdd33b3c041534d6a084cff1d53eb60ef5208f
laron58 <141530865+laron58@users.noreply.github.com>
2023-11-17 02:09:48 -0600

    Update masterCrack.py
    + Error catching
    + Updated crack loading counting values
    + LOTS of final cleaning & polishing

--- masterCrack.py
+++ masterCrack.py
@@ -1,15 +1,11 @@
 import sys
 import hashlib
 import bcrypt
-
-#count = 0
 found = False
 run = True
-#hMode = 0
 
 def main(inMode = -1):
     global count
-    global found
     global pwd
     global hashMode
     hashMode = 0
@@ -18,23 +14,28 @@ def main(inMode = -1):
     if inMode == -1:
         print("No mode given, using default setting...")
     else:
-        print("Using mode " + inMode + "...")
+        print("Using mode " + str(inMode) + "...")
     pwd = input("Enter a password: ")
+
     while run:
         count = 0
         if int(inMode) > 0:
             checkMode(inMode)
             inMode = -1
         else:
-            #print("hashMode =", hashMode)
-            mode = int(input("Enter 0 to change password\n"\
-                            "Enter 1 to use dictionary\n"\
-                            "Enter 2 to brute force\n"\
-                            "Enter 3 to convert to MD5\n"\
-                            "Enter 4 to convert to SHA256\n"\
-                            "Enter 5 to convert to BCrypt\n"\
-                            "Enter 6 to quit\n"))
-            checkMode(mode, hashMode)
+            try:
+                mode = int(input("\nEnter 0 to change password\n"\
+                                "Enter 1 to use dictionary\n"\
+                                "Enter 2 to brute force\n"\
+                                "Enter 3 to convert to MD5\n"\
+                                "Enter 4 to convert to SHA256\n"\
+                                "Enter 5 to convert to BCrypt\n"\
+                                "Enter 6 to quit\n"))
+                checkMode(mode, hashMode)
+            except ValueError:
+                print("Please enter a valid mode!")
+            except KeyboardInterrupt:
+                print("Cracking interrupted!")
             
 def dictCrack(pwd, hMode):
     global count
@@ -45,21 +46,21 @@ def dictCrack(pwd, hMode):
         if hMode == 5:
             if bcrypt.checkpw(guess.encode(), pwd):
                 print("\nCracked password:", guess)
-                print(count, "tries\n")
-                break
-            if count % 7 == 0:
+                print(count, "tries")
+                return
+            if count % 10 == 0:
                 print("Cracking...")
-        if hMode > 2:
+        elif hMode > 2:
             hGuess = toHash(guess, hMode)
         else:
             hGuess = guess
         #print(hGuess)
-        if hGuess == pwd:
+        if hMode != 5 and hGuess == pwd:
             print("\nCracked password:", guess)
-            print(count, "tries\n")
-            break
+            print(count, "tries")
+            return
     if count == 10000:
-        print("\nDictionary failed :( \nMaybe try brute forcing?\n")
+        print("\nDictionary failed :( \nMaybe try brute forcing?")
 
 def bruteCrack(pwd, size, hMode, guess = ""):
     global count
@@ -69,24 +70,25 @@ def bruteCrack(pwd, size, hMode, guess = ""):
         if hMode == 5:
             if bcrypt.checkpw(guess.encode(), pwd):
                 #print(bcrypt.checkpw(guess.encode(), pwd))
-                print("\nCracked password:" , guess)
-                print(count, "tries\n")
                 found = True
-                if found:
-                    return
-            if count % 7 == 0:
-                print("Cracking...")
-
-        elif hMode > 2:
-            guess = toHash(guess, hMode)
-        #print(guess)
-        if guess == pwd:
-            print("\nCracked password:", guess)
-            print(count, "tries\n")
-            found = True
-            if found:
+                print("\nCracked password:", guess)
+                print(count, "tries")
                 return
-        if count % 3500000 == 0:
+            if count % 10 == 0:
+                print("Cracking...")
+        elif hMode > 2:
+            hGuess = toHash(guess, hMode)
+        else:
+            hGuess = guess
+        #print(hGuess)
+        if hMode != 5 and hGuess == pwd:
+            found = True
+            print("\nCracked password:", guess)
+            print(count, "tries")
+            return
+        if hMode > 2 and count % 3000000 == 0:
+            print("Cracking...")
+        elif count % 7500000 == 0:
             print("Cracking...")
     else:
         for char in range(32, 127):
@@ -95,53 +97,48 @@ def bruteCrack(pwd, size, hMode, guess = ""):
             newGuess = guess + chr(char)
             bruteCrack(pwd, size - 1, hMode, newGuess)
 
-def bCheck(guess, hash):
-    bcrypt.check
-
 def toHash(pwd, mode):
-    global hashMode
-    hashMode = mode
     if mode == 3:
         hash = hashlib.md5(pwd.encode()).hexdigest()
     elif mode == 4:
         hash = hashlib.sha256(pwd.encode()).hexdigest()
     elif mode == 5:
-        #print(bcrypt.gensalt())
         hash = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt())
     return hash
-    
             
 def checkMode(mode, hMode = 0):
     global pwd
-    #print("hMode =", hMode)
-    if int(mode) == 0:
-        global hashMode
+    global hashMode
+    if mode == 0:
         hashMode = 0
         pwd = input("\nEnter new password: ") 
-    elif int(mode) == 1:
-        #count = 0
+    elif mode == 1:
         dictCrack(pwd, hMode)
-    elif int(mode) == 2:
-        #count = 0
+    elif mode == 2:
         global found
         found = False
         for x in range(1, 21):
             bruteCrack(pwd, x, hMode)  
-    elif int(mode) == 6:
+    elif mode == 6:
         global run
         run = False
-    else:
+    elif 2 < mode < 6:
+        hashMode = mode
         pwd = toHash(pwd, mode)
         print("\nHashed password:", pwd)
-
+    else:
+        print("Please enter a valid mode!")
 
 if __name__ == '__main__':
-    #print("~~~~CRACKERMACHINE 9000~~~~")
-    #pwd = input("Enter a password: ")
-    if len(sys.argv) > 1:
-        for i in range(1, len(sys.argv)):
-            #print(sys.argv[i])
-            mode = sys.argv[i]
-        main(mode)
-    else:
+    if len(sys.argv) == 1:
         main()
+    if len(sys.argv) == 2:
+        try:
+            if 0 < int(sys.argv[1]) < 6:
+                main(int(sys.argv[1]))
+            else:
+                print("Please enter a valid mode!")
+        except ValueError:
+            print("Please enter a valid mode!")
+    elif len(sys.argv) > 2:
+        print("Please enter only one mode!")
